1e891ae5edecbee4caeb5e40436fa053631a8b04
David Keyes <david@rfortherestofus.com>
2025-06-11 11:42:43 -0700

    Update slides

---
+++ examples/gander.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(sf)
+library(gander)
+
+portland_boundaries <- read_sf("data/portland_boundaries.geojson")
+pps_elementary_schools <- read_sf("data/pps_elementary_schools.geojson")
+
+st_join(pps_elementary_schools, portland_boundaries) %>%
+  filter(!is.na(cityname)) %>%
+  select(school)
